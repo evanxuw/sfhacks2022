67f14cb2b52dde2ce283b8b57ae643d43f3b8da0
Gaurang Ruparelia <gr2159@nyu.edu>
2022-03-12 19:04:36 -0500

    add client files for text-to-speech

--- client/src/components/Messaging.js
+++ client/src/components/Messaging.js
@@ -1,7 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 import microphoneButton from "../assets/images/microphone-button.png"
 import sendButton from "../assets/images/send-button.png"
+import Filter from "bad-words"
+import useRecorder from "../hooks/useRecorder"
+import axios from "axios"
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition
+const recognition = new SpeechRecognition()
+var current, transcript, upperCase
 const Messaging = ({ title }) => {
+  let [audioURL, isRecording, startRecording, stopRecording, audioBlob] =
+    useRecorder()
+
+  const [startedRecording, setStartedRecording] = useState(false)
+  const [text, setText] = useState()
+  const startRecord = (e) => {
+    recognition.start(e)
+    recognition.onresult = (e) => {
+      current = e.resultIndex
+      transcript = e.results[current][0].transcript
+      upperCase = transcript.charAt(0).toUpperCase() + transcript.substring(1)
+      console.log("voice event", e)
+      console.log("transcript", transcript)
+      setText(transcript)
+    }
+  }
+
+  const handleSendClick = async () => {
+    console.log(audioBlob)
+
+    let formatData = new FormData()
+    formatData.append("file", audioBlob)
+
+    const { data } = await axios.post(
+      "http://localhost:5000/transcribe",
+      formatData
+    )
+  }
+
+  var customFilter = new Filter({ placeHolder: "x" })
+
   return (
     <div className=' w-full h-screen '>
       <div className='pt-4'>
@@ -15,14 +54,40 @@ const Messaging = ({ title }) => {
           div
         </div>
       </div>
+      <audio src={audioURL} controls />
+      <div className='mx-5 py-6'>Transcript: {text}</div>
       <div className='py-5 flex flex-row px-5 '>
-        <div className='pr-4'>
+        <button
+          onClick={(e) => {
+            setStartedRecording(true)
+            startRecord(e)
+          }}
+          className='pr-4'
+        >
           <img className='cursor-pointer' src={microphoneButton} />
-        </div>
-        <input className='px-3  border-2 border-primary  w-full rounded-xl'></input>
-        <div>
+        </button>
+        <button onClick={startRecording} disabled={isRecording}>
+          start recording
+        </button>
+        <button onClick={stopRecording} disabled={!isRecording}>
+          stop recording
+        </button>
+        <input
+          placeholder={
+            startedRecording
+              ? "Press the microphone to stop recording once you're done"
+              : "Press the microphone vent out"
+          }
+          className='px-3  border-2 border-primary  w-full rounded-xl'
+        ></input>
+        <button
+          onClick={(e) => {
+            setText("")
+          }}
+        >
           <img className='cursor-pointer' src={sendButton} />
-        </div>
+        </button>
+        <button onClick={handleSendClick}>Send Audio to backend</button>
       </div>
     </div>
   )
